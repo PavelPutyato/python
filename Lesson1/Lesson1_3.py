f8d82e4ac6d59693b734482b97eae10ef04a07fe
PavelPutyato <putyato@mail.ru>
2020-04-27 15:23:49 +0300

    Выполнена домашка к первому уроку PYTHON, все выложено в папку Lesson1

---
+++ Lesson1/Lesson1_3.py
@@ -0,0 +1,7 @@
+number = int(input('Введите любое однозначное число n: '))
+number1 = number * 11
+number2 = number * 111
+number_sum = number + number1 + number2
+print('Введеное вами число n:', number)
+print(f'Сумма чисел: {number}+{number1}+{number2}={number_sum}')
+
